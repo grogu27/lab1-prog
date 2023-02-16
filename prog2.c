43e2ab1bfe2ae4b8ccadec81aaa4d482ecc53c83
Soskin Andrey <arthas@-.localdomain>
2023-02-16 21:21:42 +0700

    sourse2

---
+++ prog2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct
+{
+    char str[3];
+    int num;
+} NumberRept;
+
+void format(NumberRept* number)
+{
+    sprintf(number->str, "%3d", number->num);
+}
+
+int main()
+{
+    NumberRept number = { .num = 1025};
+
+    format(&number);
+
+    printf("str: %s\n", number.str);
+    printf("num: %d\n", number.num);
+
+    return 0;
+}
+
+
